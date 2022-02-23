eab25d5fc25f4140a8afd64f7331ea28effdf481
Leonardo Gonzalez Ciampa <99824250+lgonzalezciampa@users.noreply.github.com>
2022-02-22 21:03:29 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,32 @@
+const toDos = [];
+
+window.onload = () => {
+	const form = document.getElementById('todo-form');
+	form.onsubmit = (e) => {
+		e.preventDefault();
+		const todo = document.getElementById('todo');
+		const todoText = todo.value;
+		todo.value = ""
+		toDos.push(todoText);
+		console.log(todoText)
+		const todoList = document.getElementById('todo-list')
+
+		//con uso de map y join
+		const toDosTemplate = toDos.map(t => '<li>' + t +'</li>')
+		todoList.innerHTML = toDosTemplate.join('');
+
+
+		const elementos = document.querySelectorAll('#todo-list li')
+		elementos.forEach((elemento , i) => {
+			elemento.addEventListener('click',() =>{
+				elemento.parentNode.removeChild(elemento)
+				toDos.splice(i,1)
+			})
+		})
+		//con uso de for
+		// todoList.innerHTML = '';
+		// for(let i = 0 ; i < toDos.length ; i++){
+		// 	todoList.innerHTML += '<li>' + toDos[i] + '</li>';
+		// }
+	}
+}
