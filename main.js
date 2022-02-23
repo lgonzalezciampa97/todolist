af411976544005110540827ccd94aaf211017053
Leonardo Gonzalez Ciampa <99824250+lgonzalezciampa@users.noreply.github.com>
2022-02-22 22:39:01 -0300

    Init commit

--- main.js
+++ main.js
@@ -1,37 +1,45 @@
-const toDos = [];
+const toDos = JSON.parse(localStorage.getItem('toDos')) || [];
 
 const render = () => {
 	const todoList = document.getElementById('todo-list')
 
-		//con uso de map y join
-		const toDosTemplate = toDos.map(t => '<li>' + t +'</li>')
-		todoList.innerHTML = toDosTemplate.join('');
+	//con uso de map y join
+	const toDosTemplate = toDos.map(t => '<li>' + t +'</li>')
+	todoList.innerHTML = toDosTemplate.join('');
 
 
-		const elementos = document.querySelectorAll('#todo-list li')
-		elementos.forEach((elemento , i) => {
-			elemento.addEventListener('click',() =>{
-				elemento.parentNode.removeChild(elemento)
-				toDos.splice(i,1)
-				render()
-			})
-		})
-		//con uso de for
-		// todoList.innerHTML = '';
-		// for(let i = 0 ; i < toDos.length ; i++){
-		// 	todoList.innerHTML += '<li>' + toDos[i] + '</li>';
-		// }
-	}
-
-	window.onload = () => {
-		const form = document.getElementById('todo-form');
-		form.onsubmit = (e) => {
-			e.preventDefault();
-			const todo = document.getElementById('todo');
-			const todoText = todo.value;
-			todo.value = ""
-			toDos.push(todoText);
+	const elementos = document.querySelectorAll('#todo-list li')
+	elementos.forEach((elemento , i) => {
+		elemento.addEventListener('click',() =>{
+			elemento.parentNode.removeChild(elemento)
+			toDos.splice(i,1)
+			actualizaToDos(toDos)
 			render()
-		
+		})
+	})
+	//con uso de for
+	// todoList.innerHTML = '';
+	// for(let i = 0 ; i < toDos.length ; i++){
+	// 	todoList.innerHTML += '<li>' + toDos[i] + '</li>';
+	// }
+}
+
+const actualizaToDos = (toDos) => {
+	const todoStrings = JSON.stringify(toDos)
+	localStorage.setItem('toDos', todoStrings)
+}
+
+window.onload = () => {
+	render()
+	const form = document.getElementById('todo-form');
+	form.onsubmit = (e) => {
+		e.preventDefault();
+		const todo = document.getElementById('todo');
+		const todoText = todo.value;
+		todo.value = ""
+		toDos.push(todoText);
+		actualizaToDos(toDos)
+		render()
+
 	}
 }
