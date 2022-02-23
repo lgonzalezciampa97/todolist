126dbbe6fe757c11f8c7a166ac27d5ec9b7c1d28
Leonardo Gonzalez Ciampa <99824250+lgonzalezciampa@users.noreply.github.com>
2022-02-22 22:41:08 -0300

    init commit

--- main.js
+++ main.js
@@ -3,26 +3,26 @@ const toDos = JSON.parse(localStorage.getItem('toDos')) || [];
 const render = () => {
 	const todoList = document.getElementById('todo-list')
 
-	//con uso de map y join
-	const toDosTemplate = toDos.map(t => '<li>' + t +'</li>')
-	todoList.innerHTML = toDosTemplate.join('');
+		//con uso de map y join
+		const toDosTemplate = toDos.map(t => '<li>' + t +'</li>')
+		todoList.innerHTML = toDosTemplate.join('');
 
 
-	const elementos = document.querySelectorAll('#todo-list li')
-	elementos.forEach((elemento , i) => {
-		elemento.addEventListener('click',() =>{
-			elemento.parentNode.removeChild(elemento)
-			toDos.splice(i,1)
-			actualizaToDos(toDos)
-			render()
+		const elementos = document.querySelectorAll('#todo-list li')
+		elementos.forEach((elemento , i) => {
+			elemento.addEventListener('click',() =>{
+				elemento.parentNode.removeChild(elemento)
+				toDos.splice(i,1)
+				actualizaToDos(toDos)
+				render()
+			})
 		})
-	})
-	//con uso de for
-	// todoList.innerHTML = '';
-	// for(let i = 0 ; i < toDos.length ; i++){
-	// 	todoList.innerHTML += '<li>' + toDos[i] + '</li>';
-	// }
-}
+		//con uso de for
+		// todoList.innerHTML = '';
+		// for(let i = 0 ; i < toDos.length ; i++){
+		// 	todoList.innerHTML += '<li>' + toDos[i] + '</li>';
+		// }
+	}
 
 const actualizaToDos = (toDos) => {
 	const todoStrings = JSON.stringify(toDos)
